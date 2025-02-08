42500872d1bd24062b5ed270cdcb906f5e7853f9
Grind <mac@192.168.1.8>
2025-02-08 23:38:43 +0100

    correcting solana rpc

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 export const RPC_ENDPOINT = "https://api.mainnet-beta.solana.com";
-export const RPC_WEBSOCKET_ENDPOINT = "ws//api.mainnet-beta.solana.com";
+export const RPC_WEBSOCKET_ENDPOINT = "wss://api.mainnet-beta.solana.com";
 export const JUP_AGGREGATOR = process.env.JUP_AGGREGATOR;
 export const TARGET_WALLET = process.env.TARGET_WALLET;;
 export const MAXIMUM_BUY_AMOUNT = process.env.MAXIMUM_BUY_AMOUNT;
