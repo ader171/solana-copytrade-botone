a0599e91bb8621799d85354160cdc0af4d36149a
Grind <mac@192.168.1.8>
2025-02-08 23:34:59 +0100

    getting back to solana rpc

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
-export const RPC_ENDPOINT = "https://mainnet.helius-rpc.com/?api-key=7d3dd24c-1388-470d-b1f5-086bec6c8505";
-export const RPC_WEBSOCKET_ENDPOINT = "wss://mainnet.helius-rpc.com/?api-key=7d3dd24c-1388-470d-b1f5-086bec6c8505";
+export const RPC_ENDPOINT = "https://api.mainnet-beta.solana.com";
+export const RPC_WEBSOCKET_ENDPOINT = "ws//api.mainnet-beta.solana.com";
 export const JUP_AGGREGATOR = process.env.JUP_AGGREGATOR;
 export const TARGET_WALLET = process.env.TARGET_WALLET;;
 export const MAXIMUM_BUY_AMOUNT = process.env.MAXIMUM_BUY_AMOUNT;
