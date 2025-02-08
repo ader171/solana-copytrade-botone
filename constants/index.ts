a15cb46921aa036ad4dd6b913b7afbee59e00ea3
Grind <mac@MacBook-Pro-de-Reda.local>
2025-02-08 12:59:01 +0100

    Update all files

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,5 @@
-export const RPC_ENDPOINT = "";
-export const RPC_WEBSOCKET_ENDPOINT = "";
+export const RPC_ENDPOINT = "https://api.mainnet-beta.solana.com";
+export const RPC_WEBSOCKET_ENDPOINT = "wss://api.mainnet-beta.solana.com";
 export const JUP_AGGREGATOR = process.env.JUP_AGGREGATOR;
-export const TARGET_WALLET = process.env.TARGET_WALLET;
-export const PRICE_KEY = "";
+export const TARGET_WALLET = process.env.TARGET_WALLET;;
 export const MAXIMUM_BUY_AMOUNT = process.env.MAXIMUM_BUY_AMOUNT;
